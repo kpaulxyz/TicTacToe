1a25452f6a4958861c1a0a51dfa5c0ee447cb572
pk30686 <paul.kronner@stud.th-deg.de>
2023-04-25 23:18:54 +0200

    added turn, do_move and check_move functions

--- tictactoe.py
+++ tictactoe.py
@@ -9,6 +9,10 @@ field = [[" ", "|", " ", "|", " "],
          ["—", "+", "—", "+", "—"],
          [" ", "|", " ", "|", " "]]
 
+player1 = True  # bool that states which player is currently playing
+player1_char = "X"
+player2_char = "O"
+
 
 def printer():  # function that prints the field
 
@@ -20,16 +24,50 @@ def printer():  # function that prints the field
         print("\n", end="")
 
 
-def turn():  # function that checks whoose turn it is and where they can place their symbol
-    print("penis")
+def turn():  # function that checks whose turn it is and where they can place their symbol
+
+    while True:
+        next_move_x = int(input(
+            f"Player {1 if player1 else 2}, please choose the x (horizontal) coordinates of your next move: "))
+        if next_move_x in list(range(3)):  # TODO Implement check to see if move is legal
+            break
+        else:
+            print("Please be sure to input a valid number (0-2). \nPlease try Again.")
+
+    while True:
+        next_move_y = int(input(
+            f"Player {1 if player1 else 2}, please choose the x (vertical)   coordinates of your next move: "))
+        if next_move_y in list(range(3)):  # TODO Implement check to see if move is legal
+            break
+        else:
+            print("Please be sure to input a valid number (0-2). \nPlease try Again.")
+
+    # it might be possible to compress the two above loops into one
+
+    return check_move(next_move_x, next_move_y)
 
 
-player1_char = "X"
-player2_char = "O"
+def do_move(x, y):
+    global player1  # Tells do_move to look for the global var player1
+    field[x][y] = (player1_char if player1 else player2_char)
+    printer()
+    # TODO implement win checker here
+    player1 = not player1
+    turn()
+
+
+def check_move(x, y):
+    # translate x and y coordinates to field coordinates
+    x = int(x * 2)
+    y = int(y * 2)
+    if not field[x][y] == " ":
+        print("Your given coordinates are already occupied. \nPlease Try again.\n")
+        turn()
+    else:
+        do_move(x, y)
+
 
 print("Willkommen zu Kat&Paul's TicTacToe:")
-
 printer()
-
-
-# ssh push test
+print("Player 1 (X) will start playing.")
+turn()
